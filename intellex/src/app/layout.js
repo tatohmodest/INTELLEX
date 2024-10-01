265377771bf101f4df9264b0b5e2c60abfd0fba8
NurBlessedProj <blessednur67@gmail.com>
2024-10-01 11:15:40 +0100

    details page done with a single modification to the layout.js

--- intellex/src/app/layout.js
+++ intellex/src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       
       <body className={classes.body}>
-        <Header />
+        {/* <Header /> */}
         {children}
         </body>
     </html>
